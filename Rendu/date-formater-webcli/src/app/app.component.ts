55d80f4932d9a2b48a590d6e48b2d1bf774cf1ec
Thifenn Cartayrade <thifenn.cartayrade@etu.univ-grenoble-alpes.fr>
2023-03-02 14:51:19 +0100

    add some coms in angular folder but switch on symfony code

--- Rendu/date-formater-webcli/src/app/app.component.ts
+++ Rendu/date-formater-webcli/src/app/app.component.ts
@@ -7,7 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'date-formater-webcli';
+  date = Date;
+  xmlhttp = new XMLHttpRequest();
+  URL = 'http://localhost:4200';
+
+  sendAndDispDate(){
+    alert(this.date);
+    this.xmlhttp.open("GET", this.URL);
+    this.xmlhttp.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+    this.xmlhttp.send(JSON.stringify(this.date));
+  }
+
+  getDate(event: any){
+    this.date = event.target.value;
+  }
 }
+
 
 /**
  * Requête post et get inutile puisqu'on a pas de BDD
